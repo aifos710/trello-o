6d3e41cf66f3262c33076e7eba314ae3fd54aa7e
aifos710 <sofia.m.caballero@gmail.com>
2016-10-08 17:24:37 -0500

    0.dos

--- js/app.js
+++ js/app.js
@@ -17,6 +17,27 @@
 		this.classList.add("none" ); // span
 		this.nextElementSibling.classList.remove("none"); // formulario
 		this.parentElement.classList.add("bgContenedor"); // contenedor
+
+		this.nextElementSibling.lastElementChild.addEventListener("click", agregarTituloLista); //btn guardar
+		this.nextElementSibling.lastElementChild.addEventListener("click", agregarNuevaLista); // btn guardar
+	};	
+
+	function agregarTituloLista(e){
+		e.preventDefault();
+		formulario.classList.add("none");
+
+		var padreContenedor = this.parentElement.parentElement; //boton,form,contenedor
+
+		var tituloLista = document.createElement("div");
+		tituloLista.textContent= input.value; //titulo de tarjeta nueva
+		padreContenedor.appendChild(tituloLista).classList.add("tituloLista");
+		input.value = "";
+
+		var cajaAnadir = document.createElement("div");
+		cajaAnadir.textContent = "Añadir una tarjeta..."
+		padreContenedor.appendChild(cajaAnadir).classList.add("cajaAnadir");
+
+		cajaAnadir.addEventListener("click", agregarTarjeta);
 	};	
 
 })();
